02535fa053132b6038a425010d4bb1c984b249a0
DannyBoyNYC <daniel.deverell@gmail.com>
2018-10-05 15:30:33 -0400

    done

--- 4-top-stories/scripts.js
+++ 4-top-stories/scripts.js
@@ -1,45 +1,16 @@
 // https://courses.gomakethings.com/academy/fall-2018/injecting-html-into-the-dom/
 
 var elem = document.querySelector('#app');
+// var text = elem.textContent;
+
 var nytapi = 'd7d88f32a04d4c6aab4e46735441d0ee';
 var nytDocumentation = 'https://developer.nytimes.com/top_stories_v2.json';
 var categories = ['food', 'fashion', 'travel'];
 var limit = 3;
 var topStories = '';
 
-var xhr = new XMLHttpRequest();
-
-// methods
-
-var sanitizeHTML = function (str) {
-  var temp = document.createElement('div');
-  temp.textContent = str;
-  return temp.innerHTML;
-};
-
-xhr.onreadystatechange = function () {
-  console.log('ran')
-  if (xhr.readyState !== 4) return;
-  if (xhr.status >= 200 && xhr.status < 300) {
-    var data = (JSON.parse(xhr.responseText));
-  } else {
-    console.log('The request failed!');
-  }
-  processFeed(data)
-
-};
-
-var getFeed = function(cat){
-  xhr.open('GET', 'https://api.nytimes.com/svc/topstories/v2/'+cat+'.json?api-key='+nytapi);
-  xhr.send();
-  console.log(cat)
-}
-
-var processFeed = function (data) {
-  
-  var stories = data.results.slice(0, limit);
+var processFeed = function (stories) {
   // console.log(stories);
-  // var category = document.createElement('h2');
   stories.forEach(function (story) {
     // console.log(story);
     var storyEl = document.createElement('div');
@@ -47,24 +18,46 @@ var processFeed = function (data) {
     storyEl.innerHTML = `
     <img style="float:left;" src="${story.multimedia[0].url}" /> 
     <h3>
-    <a target="_blank" href="${story.short_url}">${sanitizeHTML(story.title)}</a>
+    <a target="_blank" href="${story.short_url}">${story.title}</a>
     </h3>
     <p>${story.abstract}</p>
     `;
-    
-    var target = document.querySelector('#app');
-    target.prepend(storyEl);
-    
+    console.log(storyEl);
+    elem.textContent += storyEl;
+    // return story;
   })
-  console.log(topStories);
+  // console.log(elem.textContent);
 }
 
+var makeRequest = function(url, method) {
+  var request = new XMLHttpRequest();
+  return new Promise(function (resolve, reject) {
+    request.onreadystatechange = function () {
+      if (request.readyState !== 4) return;
+      if (request.status >= 200 && request.status < 300) {
+        // successful
+        resolve(request);
+      } else {
+        // failed
+        reject({
+          status: request.status,
+          statusText: request.statusText
+        });
+      }
+    };
+    request.open(method || 'GET', url, true);
+    request.send();
+  });
+}
 
+makeRequest('https://api.nytimes.com/svc/topstories/v2/food.json?api-key=' + nytapi)
+  .then(function (posts) {
+    var data = (JSON.parse(posts.responseText));
+    var stories = data.results.slice(0, limit);
+    // console.log(posts); // returns XMLHttpRequest
+    processFeed(stories)
 
-
-categories.forEach(function (category) { 
-  getFeed(category);
-})
-
-{/* <img src="${story.multimedia[0].url}" />  */ }
-{/* <date>${story.published_date}</date> */}
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
