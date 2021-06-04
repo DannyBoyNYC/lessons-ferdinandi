6dfdd122440d5105326fb89ef2a4d7475786b1b5
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-04 17:37:06 -0400

    day 5 - multiple forms

--- c-multiple-forms/scripts.js
+++ c-multiple-forms/scripts.js
@@ -1,25 +1,28 @@
-const passwordCheckboxes = document.querySelectorAll('[type="checkbox"]')
-const [...passwordFields] = document.querySelectorAll('[type="password"]')
+const forms = document.querySelectorAll('form')
+const inputs = document.querySelectorAll('input, select, textarea')
 
-console.log('passwordFields:::  ', passwordFields)
-passwordFields.forEach((f) => console.log('  ', f.id))
+function togglePasswords(event) {
+  if (!event.target.matches('[data-password]')) return
 
-function toggleDisplayPasswords() {
-  if (event.target.matches('[name="show-password"]')) {
-    let tempe = passwordFields.filter((field) => {
-      console.log('  ', field.id)
-      field.id === '#password'
-    })
-    console.log(' tempe ', tempe)
-    passwordFields[0].type =
-      passwordFields[0].type === 'password' ? 'text' : 'password'
-  }
-  if (event.target.matches('[name="show-passwords"]')) {
-    passwordFields[1].type =
-      passwordFields[1].type === 'password' ? 'text' : 'password'
-    passwordFields[2].type =
-      passwordFields[2].type === 'password' ? 'text' : 'password'
+  const passwords = document.querySelectorAll(
+    event.target.getAttribute('data-password')
+  )
+
+  for (let pw of passwords) {
+    pw.type = event.target.checked ? 'text' : 'password'
   }
 }
 
-document.addEventListener('click', toggleDisplayPasswords)
+for (let input of inputs) {
+  // the invalid event fires before submit
+  // add error class
+  input.addEventListener('invalid', () => {
+    input.classList.add('error')
+  })
+
+  input.addEventListener('blur', () => {
+    input.checkValidity()
+  })
+}
+
+document.addEventListener('change', togglePasswords)
