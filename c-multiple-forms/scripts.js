af7e5de656e2df8f80e9360b94dd856adee491e0
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-05 15:03:26 -0400

    multiple forms

--- c-multiple-forms/scripts.js
+++ c-multiple-forms/scripts.js
@@ -3,9 +3,14 @@ const inputs = document.querySelectorAll('input, select, textarea')
 function togglePasswords(event) {
   if (!event.target.matches('[data-password]')) return
 
-  const passwords = document.querySelectorAll(
-    event.target.getAttribute('data-password')
-  )
+  // My original solution
+  // const passwords = document.querySelectorAll(
+  //   event.target.getAttribute('data-password')
+  // )
+
+  // Chris' solution - probably just more readable
+  const selector = event.target.getAttribute('data-password')
+  const passwords = document.querySelectorAll(selector)
 
   for (let pw of passwords) {
     pw.type = event.target.checked ? 'text' : 'password'
@@ -14,7 +19,7 @@ function togglePasswords(event) {
 
 for (let input of inputs) {
   // the invalid event fires before submit
-  // add error class
+  // +++ add error class
   input.addEventListener('invalid', () => {
     input.classList.add('error')
   })
