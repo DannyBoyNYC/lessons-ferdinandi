2dfb6ee7689172680f94b408e2539a4c9e4f7aab
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-09 14:46:49 -0400

    done

--- e-text-word-count/scripts.js
+++ e-text-word-count/scripts.js
@@ -1,6 +1,7 @@
 const textInput = document.querySelector('#text-input')
 const characterCount = document.querySelector('#character-count')
 const numberCount = document.querySelector('#number-count')
+const wordCount = document.querySelector('#word-count')
 
 const regex = /[0-9]/g
 
@@ -9,8 +10,18 @@ function showCharacterCount(event) {
   characterCount.innerText = textInput.value.length
 
   let numberOfNumbers = textInput.value.match(regex)
-  console.log(' numberOfNumbers:: ', numberOfNumbers)
   numberCount.innerText = numberOfNumbers === null ? 0 : numberOfNumbers.length
+  showWordCount()
+}
+
+function showWordCount() {
+  console.log(' textInput.innerText ', textInput.value)
+
+  let numWords = textInput.value
+    .split(/[\n\r\s]+/g)
+    .filter((word) => word.length > 0)
+
+  wordCount.innerText = numWords.length
 }
 
 document.addEventListener('input', showCharacterCount)
