0242487799b5b5cc5453d7f13cd6aa56bdaa1d02
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-15 09:12:02 -0400

    refactor

---
+++ g-promises/best/scripts.js
@@ -0,0 +1,83 @@
+/* ====================================================
+  Variables
+  ==================================================== */
+const blockquote = document.querySelector('blockquote')
+const endpoint = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
+
+/* ====================================================
+    Functions
+    ==================================================== */
+
+/* Helpers
+  /* ==================================================== */
+/**
+ * Handle any errors
+ * @param      {object}  error   The error object
+ */
+function errorHandler(error) {
+  // Fall back text and quote
+  blockquote.innerHTML = `
+      <p>Sorry, we can't get any more Ron right now. In his own words:</p>
+      <p><strong>You can't hack into a typewriter. That's all I have to say.</strong></p>
+    `
+  // Add error to console
+  console.error(error)
+}
+
+/**
+ * Converts response from an API to a JSON object
+ * @param  {string} response Unprocessed response from request
+ * @return {array}           Response converted to JSON or rejected promise
+ */
+function convertToJSON(response) {
+  return response.ok
+    ? response.json()
+    : Promise.reject(new Error(response.status))
+}
+
+/* Library
+  /* ==================================================== */
+/**
+ * Render the returned data to HTML
+ * @param      {string}  data    The data from the API
+ */
+function renderQuote(data) {
+  blockquote.innerHTML = `<p>${data}</p>`
+}
+
+/**
+ * Fetches quotes from the API
+ */
+function fetchQuotes() {
+  fetch(endpoint) //
+    .then(convertToJSON) //
+    .then(renderQuote) //
+    .catch(errorHandler)
+}
+
+/* Handlers
+  /* ==================================================== */
+
+/**
+ * Handle inputs to render the number of words and characters in the HTML
+ */
+function inputHandler(event) {
+  if (!event.target.matches('button')) return
+  fetchQuotes()
+}
+
+/* ====================================================
+    Inits and Event Listeners
+    ==================================================== */
+
+/**
+ * Render HTML that is dependent on JavaScript
+ */
+function renderButton() {
+  const button = document.createElement('button')
+  button.textContent = 'More Ron!'
+  blockquote.after(button)
+}
+
+renderButton()
+document.addEventListener('click', inputHandler)
--- g-promises/scripts.js
+++ g-promises/scripts.js
@@ -3,28 +3,50 @@ const btn = document.querySelector('#get-quote')
 const blockQuote = document.querySelector('blockquote')
 const collectedWisdom = document.querySelector('#collected-wisdom')
 
-var ronsWisdom = []
+let ronsWisdom = []
+
+function convertToJSON(response) {
+  return response.ok
+    ? response.json()
+    : Promise.reject(new Error(response.status))
+}
+
+function renderQuote(data) {
+  ronsWisdom.push(data[0])
+  blockQuote.innerText = ronsWisdom[ronsWisdom.length - 1]
+  collectedWisdom.innerHTML = ronsWisdom
+    .map((wise) => `<p>${wise}</p>`)
+    .join('')
+}
+
+function errorHandler(error) {
+  blockQuote.innerHTML = `Sorry, something went wrong.`
+  console.error(error)
+}
 
 function getQuote() {
   fetch(API)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json()
-      }
-      return response.json().then(function (json) {
-        throw json
-      })
-    })
-    .then(function (data) {
-      ronsWisdom.push(data[0])
-      blockQuote.innerText = ronsWisdom[ronsWisdom.length - 1]
-      collectedWisdom.innerHTML = ronsWisdom
-        .map((wise) => `<p>${wise}</p>`)
-        .join('')
-    })
-    .catch(function (error) {
-      console.warn(error)
-    })
+    .then(convertToJSON)
+    // .then(function (response) {
+    //   if (response.ok) {
+    //     return response.json()
+    //   }
+    //   return response.json().then(function (json) {
+    //     throw json
+    //   })
+    // })
+    .then(renderQuote)
+    // .then(function (data) {
+    //   ronsWisdom.push(data[0])
+    //   blockQuote.innerText = ronsWisdom[ronsWisdom.length - 1]
+    //   collectedWisdom.innerHTML = ronsWisdom
+    //     .map((wise) => `<p>${wise}</p>`)
+    //     .join('')
+    // })
+    .catch(errorHandler)
+  // .catch(function (error) {
+  //   console.warn(error)
+  // })
 }
 
 btn.addEventListener('click', getQuote)
