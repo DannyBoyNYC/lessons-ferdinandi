3e90a6a75d8c969e5b0503db2aed6eab0c8ca772
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-16 09:27:45 -0400

    done

---
+++ h-arrays/scripts.js
@@ -0,0 +1,40 @@
+// Get the blockquote and button elements
+let quote = document.querySelector('#quote')
+let btn = document.querySelector('#get-quote')
+
+let ronsWisdom = []
+let numberOfQuotes = 0
+let maxNumberOfQuotes = 50
+
+function checkAndSet(data) {
+  numberOfQuotes++
+  if (ronsWisdom.includes(data[0]) && numberOfQuotes <= maxNumberOfQuotes) {
+    console.log(' dupe detected ', data[0])
+    getQuote()
+  } else {
+    ronsWisdom.push(data[0])
+    return (quote.textContent = ronsWisdom[ronsWisdom.length - 1])
+  }
+}
+
+// Get a fresh quote and render it into the DOM
+function getQuote() {
+  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+    .then(function (response) {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response.status
+    })
+    .then(checkAndSet)
+    .catch(function (error) {
+      quote.textContent =
+        '[Something went wrong, sorry!] I have a joke for you... The government in this town is excellent, and uses your tax dollars efficiently.'
+    })
+}
+
+// Get a quote on page load
+getQuote()
+
+// Get a quote when the #get-quote button is clicked
+btn.addEventListener('click', getQuote)
