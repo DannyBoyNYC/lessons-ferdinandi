c763987fa8e36ce2e42fb78486af7fabb37671a9
Daniel Deverell <daniel.deverell@gmail.com>
2021-07-05 22:09:56 -0400

    done

--- b-toggle-multiple-passwords/scripts.js
+++ b-toggle-multiple-passwords/scripts.js
@@ -1,41 +1,41 @@
-;(function (window, document, undefined) {
-  const loginForm = document.querySelector('#login')
-  const passwordFields = loginForm.querySelectorAll('[type="password"]')
-  const togglePasswordsCheckbox = loginForm.querySelector('#show-passwords')
+(function (window, document, undefined) {
+  const loginForm = document.querySelector('#login');
+  const passwordFields = loginForm.querySelectorAll('[type="password"]');
+  const togglePasswordsCheckbox = loginForm.querySelector('#show-passwords');
 
-  const [button1, button2] = document.querySelectorAll('[type="password"]')
-  console.log('desructured:::', button1, button2)
+  const [button1, button2] = document.querySelectorAll('[type="password"]');
+  console.log('desructured:::', button1, button2);
 
   function handleChange(e) {
-    if (!e.target.matches('#show-passwords')) return
+    if (!e.target.matches('#show-passwords')) return;
     for (let passwordField of passwordFields) {
       if (togglePasswordsCheckbox.checked) {
-        passwordField.type = 'text'
+        passwordField.type = 'text';
       } else {
-        passwordField.type = 'password'
+        passwordField.type = 'password';
       }
     }
   }
 
   function handleInput(e) {
-    if (!e.target.matches('[type="password"]')) return
-    toggleDisablePasswordCheckbox()
+    if (!e.target.matches('[type="password"]')) return;
+    toggleDisablePasswordCheckbox();
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    loginForm.reset()
-    togglePasswordsCheckbox.disabled = true
+    e.preventDefault();
+    loginForm.reset();
+    togglePasswordsCheckbox.disabled = true;
   }
 
   function toggleDisablePasswordCheckbox() {
     for (let passwordField of passwordFields) {
       togglePasswordsCheckbox.disabled =
-        passwordField.value.length > 0 ? false : true
+        passwordField.value.length > 0 ? false : true;
     }
   }
 
-  document.addEventListener('change', handleChange)
-  document.addEventListener('input', handleInput)
-  document.addEventListener('submit', handleSubmit)
-})(window, document)
+  document.addEventListener('change', handleChange);
+  document.addEventListener('input', handleInput);
+  document.addEventListener('submit', handleSubmit);
+})(window, document);
